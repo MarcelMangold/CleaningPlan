c22af3f1ae64fd6d42fa9dc48a330c67e3f0bb79
Lightknight <lolxDx@web.de>
2019-04-01 21:40:42 +0200

    add todo's + pages + edit home site

--- cleaningApp/src/app/app-routing.module.ts
+++ cleaningApp/src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'today', loadChildren: './today/today.module#TodayPageModule' },
+  { path: 'week', loadChildren: './week/week.module#WeekPageModule' },
+  { path: 'month', loadChildren: './month/month.module#MonthPageModule' }
 ];
 @NgModule({
   imports: [
