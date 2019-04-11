310502ae5397de6b9f228ddbe905840a8f1a6ed8
MarcelMangold <marcel.gold@outlook.com>
2019-04-11 21:52:16 +0200

    add + customizing calendar and fix tar security problem

--- app/app/src/app/pages/home/home.module.ts
+++ app/app/src/app/pages/home/home.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { NgCalendarModule  } from 'ionic2-calendar';
 
 import { HomePage } from './home.page';
 
 @NgModule({
   imports: [
     CommonModule,
+    NgCalendarModule,
     FormsModule,
     IonicModule,
     RouterModule.forChild([
--- app/app/src/app/pages/home/home.page.ts
+++ app/app/src/app/pages/home/home.page.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+
+
 
 @Component({
   selector: 'app-home',
@@ -7,10 +9,143 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
+    eventSource;
+    viewTitle;
+    currentHour;
+
+    isToday:boolean;
+ 
+    calendar = {
+        mode: 'month',
+        currentDate: new Date(),
+        dateFormatter: {
+            formatMonthViewDay: function(date:Date) {
+                return date.getDate().toString();
+            },
+            formatMonthViewDayHeader: function(date:Date) {
+                return date.toLocaleString('de-DE', {  
+                    weekday: "short",
+                });               
+            },
+            formatMonthViewTitle: function(date:Date) {
+                return date.toLocaleString('de-DE', {  
+                    month: "short",
+                }); 
+            },
+            formatWeekViewDayHeader: function(date:Date) {
+                return date.toLocaleString('de-DE', {  
+                    day: "2-digit",
+                }); 
+            },
+            formatWeekViewTitle: function(date:Date) {
+                return date.toLocaleString('de-DE', {  
+                    month: "short",
+                }) + " KW"; 
+            },
+            formatWeekViewHourColumn: function(date:Date) {
+                return date.toLocaleString('de-DE', {  
+                    hour: "2-digit",
+                }); 
+            },
+            formatDayViewHourColumn: function(date:Date) {
+                return date.toLocaleString('de-DE', {  
+                    hour: "2-digit",
+                }); 
+            },
+            formatDayViewTitle: function(date:Date) {
+                return date.toLocaleString('de-DE', {  
+                    day: "2-digit",
+                }); 
+            }
+        }
+};
 
     constructor(private authService: AuthService) { }
 
+ 
+
+    loadEvents() {
+        this.eventSource = this.createRandomEvents();
+    }
+
+    onViewTitleChanged(title) {
+        this.viewTitle = title;
+    }
+
+    onEventSelected(event) {
+        console.log('Event selected:' + event.startTime + '-' + event.endTime + ',' + event.title);
+    }
+
+    changeMode(mode) {
+        this.calendar.mode = mode;
+    }
+
+    today() {
+        this.calendar.currentDate = new Date();
+    }
+
+    onTimeSelected(ev) {
+        console.log('Selected time: ' + ev.selectedTime + ', hasEvents: ' +
+            (ev.events !== undefined && ev.events.length !== 0) + ', disabled: ' + ev.disabled);
+    }
+
+    onCurrentDateChanged(event:Date) {
+        var today = new Date();
+        today.setHours(0, 0, 0, 0);
+        event.setHours(0, 0, 0, 0);
+        this.isToday = today.getTime() === event.getTime();
+    }
+
+    createRandomEvents() {
+        var events = [];
+        for (var i = 0; i < 50; i += 1) {
+            var date = new Date();
+            var eventType = Math.floor(Math.random() * 2);
+            var startDay = Math.floor(Math.random() * 90) - 45;
+            var endDay = Math.floor(Math.random() * 2) + startDay;
+            var startTime;
+            var endTime;
+            if (eventType === 0) {
+                startTime = new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + startDay));
+                if (endDay === startDay) {
+                    endDay += 1;
+                }
+                endTime = new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + endDay));
+                events.push({
+                    title: 'All Day - ' + i,
+                    startTime: startTime,
+                    endTime: endTime,
+                    allDay: true
+                });
+            } else {
+                var startMinute = Math.floor(Math.random() * 24 * 60);
+                var endMinute = Math.floor(Math.random() * 180) + startMinute;
+                startTime = new Date(date.getFullYear(), date.getMonth(), date.getDate() + startDay, 0, date.getMinutes() + startMinute);
+                endTime = new Date(date.getFullYear(), date.getMonth(), date.getDate() + endDay, 0, date.getMinutes() + endMinute);
+                events.push({
+                    title: 'Event - ' + i,
+                    startTime: startTime,
+                    endTime: endTime,
+                    allDay: false
+                });
+            }
+        }
+        return events;
+    }
+
+    onRangeChanged(ev) {
+        console.log('range changed: startTime: ' + ev.startTime + ', endTime: ' + ev.endTime);
+    }
+
+    markDisabled = (date:Date) => {
+        var current = new Date();
+        current.setHours(0, 0, 0);
+        return date < current;
+};
+
     ngOnInit() {
+        let date = new Date()
+        this.currentHour = date.getHours();
     }
 
 
